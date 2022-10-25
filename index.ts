819c13ef0f8c58936370d3c01021d99c731e47d3
Kevin Fairise <kevin.fairise@student-cs.fr>
2022-10-25 11:35:49 +0200

    update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,24 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
+const WORD_TO_FIND = "bucheron";
+
+type PolytechResponse = {
+    result: string;
+    simscore: number;
+};
+
 async function handler(_req: Request): Promise<Response> {
 
+    const guess = _req;
+    console.log(_req);
     const payload = {sim1: "chat", sim2: "chien", lang: "fr", type: "General Word2Vec"}
 
-    const res: Promise<Response> = fetch("http://nlp.polytechnique.fr/similarityscore", {method: "POST", body: JSON.stringify(payload)})
+    const res: Promise<Response> = fetch("http://nlp.polytechnique.fr/similarityscore", {method: "POST", body: JSON.stringify(payload), headers: {"Content-Type": "application/json"}})
 
-  return new Response((await res).body);
+
+    const reponse : PolytechResponse = await res.then((res) => res.json())
+
+    return new Response("Similarité entre " + payload.sim1 + " et " + payload.sim2 + " : " + reponse.simscore);
 }
 
 serve(handler);
