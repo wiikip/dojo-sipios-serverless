b05030e769eaf4ba5f7e748d969e9e63f37acbae
Kevin Fairise <kevin.fairise@student-cs.fr>
2022-10-25 11:37:44 +0200

    update index.ts

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ type PolytechResponse = {
 async function handler(_req: Request): Promise<Response> {
 
     const guess = _req;
-    console.log(_req);
+    console.log(_req.body);
     const payload = {sim1: "chat", sim2: "chien", lang: "fr", type: "General Word2Vec"}
 
     const res: Promise<Response> = fetch("http://nlp.polytechnique.fr/similarityscore", {method: "POST", body: JSON.stringify(payload), headers: {"Content-Type": "application/json"}})
