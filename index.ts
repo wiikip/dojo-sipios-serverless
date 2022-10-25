ee78d53b2289c39f658f85624f1138da767dd4d8
Kevin Fairise <kevin.fairise@student-cs.fr>
2022-10-25 11:12:47 +0200

    update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-  return new Response("Hello World");
+
+    const payload = {sim1: "chat", sim2: "chien", lang: "fr", type: "General Word2Vec"}
+
+    const res: Promise<Response> = fetch("http://nlp.polytechnique.fr/similarityscore", {method: "POST", body: JSON.stringify(payload)})
+
+  return new Response((await res).body);
 }
 
 serve(handler);
